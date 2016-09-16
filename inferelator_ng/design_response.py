2f611a3769f90540f88bbf3f88051c8807a86177
Eliza Chang <eliza.j.chang@gmail.com>
2016-09-16 13:07:21 -0400

    minor changes

--- inferelator_ng/design_response.py
+++ inferelator_ng/design_response.py
@@ -6,35 +6,46 @@ import pandas as pd
 import math
 
 
-def is_nan(x):
-    if type(x) is float:
-        return math.isnan(x)
-    else:
-        return False
+class DesignResponseCalculator:
+    """
+    Class to hold typical arguments when wanting to calculate design and response matrices.
+    """
+    def __init__(self,
+                 metadata_file,
+                 expression_file,
+                 response_file="response.tsv",
+                 design_file="design.tsv",
+                 del_t_min=0,
+                 del_t_max=110,
+                 tau=45):
+        # TO DO: do path checks / creation
+        self.metadata_df = load_metadata_df(metadata_file)
+        self.expression_df = load_expression_df(expression_file)
+        self.response_file = response_file
+        self.design_file = design_file
+        self.del_t_min = del_t_min
+        self.del_t_max = del_t_max
+        self.tau = tau
 
+    def run(self):
+        expression_df = pd.read_csv(self.expression_file,
+                                    sep='\t')
+        metadata_df = pd.read_csv(self.metadata_file,
+                                  sep='\t')
+        metadata_df.columns = metadata_df.columns.apply(metadata_tsv_column_map)
 
-def metadata_tsv_column_map(column_name):
-    column_map = {"isTs" : "is_ts",
-                  "is1stLast" : "is_first_last",
-                  "prevCol" : "prev_col",
-                  "del.t" : "del_t",
-                  "condName" : "condition_name"}
-    return column_map[column_name]
+        # design.and.response(meta.data, exp.mat, delT.min, delT.max, tau)
+        design, response = calculate_design_and_response(metadata_df,
+                                                         expression_df,
+                                                         self.del_t_min,
+                                                         self.del_t_max,
+                                                         self.tau)
 
+        # TO DO: proper `os.path` wrappers to write these to the correct place.
+        design.to_csv(self.design_file)
+        response.to_csv(self.response_file)
 
-def load_metadata_df(metadata_file):
-    metadata_df = pd.read_csv(metadata_file, sep=',', index_col=0, dtype={"del.t": float})
-    metadata_df.columns = [metadata_tsv_column_map(x) for x in metadata_df.columns]
-    return metadata_df
-
-
-def load_expression_df(expression_file):
-    expression_df = pd.read_csv(expression_file, sep=',', index_col=0)
-    return expression_df
-
-
-def calculate_design_and_response(self,
-                                  metadata_df,
+def calculate_design_and_response(metadata_df,
                                   expression_df,
                                   del_t_min,
                                   del_t_max,
@@ -85,43 +96,28 @@ def calculate_design_and_response(self,
     return design, response
 
 
-class DesignResponseCalculator:
-    """
-    Class to hold typical arguments when wanting to calculate design and response matrices.
-    """
-    def __init__(self,
-                 metadata_file,
-                 expression_file,
-                 response_file="response.tsv",
-                 design_file="design.tsv",
-                 del_t_min=0,
-                 del_t_max=110,
-                 tau=45):
-        # TO DO: do path checks / creation
-        self.metadata_df = load_metadata_df(metadata_file)
-        self.expression_df = load_expression_df(expression_file)
-        self.response_file = response_file
-        self.design_file = design_file
-        self.del_t_min = del_t_min
-        self.del_t_max = del_t_max
-        self.tau = tau
+def is_nan(x):
+    if type(x) is float:
+        return math.isnan(x)
+    else:
+        return False
 
-    def run(self):
-        expression_df = pd.read_csv(self.expression_file,
-                                    sep='\t')
 
-        metadata_df = pd.read_csv(self.metadata_file,
-                                  sep='\t')
-        metadata_df.columns = metadata_df.columns.apply(metadata_tsv_column_map)
+def metadata_tsv_column_map(column_name):
+    column_map = {"isTs" : "is_ts",
+                  "is1stLast" : "is_first_last",
+                  "prevCol" : "prev_col",
+                  "del.t" : "del_t",
+                  "condName" : "condition_name"}
+    return column_map[column_name]
 
-        # design.and.response(meta.data, exp.mat, delT.min, delT.max, tau)
-        design, response = calculate_design_and_response(metadata_df,
-                                                         expression_df,
-                                                         self.delTmin,
-                                                         self.delTmax,
-                                                         self.tau)
 
-        # TO DO: proper `os.path` wrappers to write these to the correct place.
-        design.to_csv(self.design_file)
-        response.to_csv(self.response_file)
+def load_metadata_df(metadata_file):
+    metadata_df = pd.read_csv(metadata_file, sep=',', index_col=0, dtype={"del.t": float})
+    metadata_df.columns = [metadata_tsv_column_map(x) for x in metadata_df.columns]
+    return metadata_df
 
+
+def load_expression_df(expression_file):
+    expression_df = pd.read_csv(expression_file, sep=',', index_col=0)
+    return expression_df
